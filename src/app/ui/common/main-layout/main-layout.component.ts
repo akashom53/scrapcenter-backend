e0fe2d79a994f19d76ad3073d135546daafb627f
Akash Srivastava <akashom53@gmail.com>
2025-05-25 04:08:52 +0530

    cleanup

--- src/app/ui/common/main-layout/main-layout.component.ts
+++ src/app/ui/common/main-layout/main-layout.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from "../sidebar/sidebar.component";
 import { RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [SidebarComponent, RouterOutlet],
+  imports: [SidebarComponent, RouterOutlet, NgIf],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css'
 })
 export class MainLayoutComponent {
+  showUserDropdown = false;
+  toggleUserDropdown() {
+    this.showUserDropdown = !this.showUserDropdown;
+  }
 
 }
--- src/app/ui/pages/home/home.component.ts
+++ src/app/ui/pages/home/home.component.ts
@@ -1,26 +1,19 @@
 import { Component, inject } from '@angular/core';
-import { AuthService } from '../../../auth/auth.service';
-import { NgIf } from '@angular/common';
-import { SidebarComponent } from "../../common/sidebar/sidebar.component";
 import { AuthStore } from '../../../auth/state/auth.store';
 import { AppStore } from '../../../state/app.store';
 
 @Component({
-  imports: [NgIf, SidebarComponent],
+  imports: [],
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  showUserDropdown = false;
   private authStore = inject(AuthStore)
   private appStore = inject(AppStore)
 
-  constructor(private authService: AuthService) { }
 
-  toggleUserDropdown() {
-    this.showUserDropdown = !this.showUserDropdown;
-  }
+
 
   handleNewClick() {
     this.appStore.navigate('/new')
